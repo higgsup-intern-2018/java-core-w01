998a910993c9a0af999556714900f75e169ab865
Thieu Tu <tu.thieuthanh@gmail.com>
2018-06-15 16:01:14 +0700

    Complete Exercise 01!

---
+++ src/main/java/com/higgsup/intern/w01/sec01/Exercise01.java
@@ -0,0 +1,10 @@
+package com.higgsup.intern.w01.sec01;
+
+//prints "Hello, World" ten times.
+public class Exercise01 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10 ; i++){
+            System.out.println("Hello, World");
+        }
+    }
+}
