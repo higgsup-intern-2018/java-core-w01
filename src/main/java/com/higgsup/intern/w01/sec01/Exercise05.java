925ab593aff4fe65d0fd0d0036a663cc7bf4ce54
Thieu Tu <tu.thieuthanh@gmail.com>
2018-06-15 16:39:08 +0700

    Complete Exercise 05!

---
+++ src/main/java/com/higgsup/intern/w01/sec01/Exercise05.java
@@ -0,0 +1,13 @@
+package com.higgsup.intern.w01.sec01;
+
+public class Exercise05 {
+    public static void main(String[] args) {
+        System.out.print("Hi ");
+        System.out.print(args[2]);
+        System.out.print(", ");
+        System.out.print(args[1]);
+        System.out.print(", and ");
+        System.out.print(args[0]);
+        System.out.print(".");
+    }
+}
