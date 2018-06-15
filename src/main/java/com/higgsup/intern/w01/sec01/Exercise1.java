6340a5111a5df349458eac492a6f4960863a3c86
tuanhiep <nguyennhutuanhiep@gmail.com>
2018-06-15 16:40:52 +0700

    up bai ve nha

--- src/main/java/com/higgsup/intern/w01/sec01/Exercise1.java
+++ src/main/java/com/higgsup/intern/w01/sec01/Exercise1.java
@@ -1,4 +1,18 @@
 package com.higgsup.intern.w01.sec01;
 
-public class Exercise1 {
+public class Exercise1
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello, world");
+        System.out.println("Hello, world");
+        System.out.println("Hello, world");
+        System.out.println("Hello, world");
+        System.out.println("Hello, world");
+        System.out.println("Hello, world");
+        System.out.println("Hello, world");
+        System.out.println("Hello, world");
+        System.out.println("Hello, world");
+        System.out.println("Hello, world");
+    }
 }
