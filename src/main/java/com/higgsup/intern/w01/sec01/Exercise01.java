6fb52d1236c028747483e4dba3ad5082d58c74ca
DucThanh6111996 <Hoahongden123>
2018-06-15 16:08:57 +0700

    bai1

--- src/main/java/com/higgsup/intern/w01/sec01/Exercise01.java
+++ src/main/java/com/higgsup/intern/w01/sec01/Exercise01.java
@@ -1,4 +1,9 @@
 package com.higgsup.intern.w01.sec01;
 
 public class Exercise01 {
+    public static void main(String[] args){
+        for(int i=1 ; i<10 ;i++){
+            System.out.println("Hello,World");
+        }
+    }
 }
