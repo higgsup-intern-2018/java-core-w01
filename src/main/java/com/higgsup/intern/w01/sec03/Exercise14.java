cc2512daab7f424b5f3fb840be7a55371e7670c9
tuanhiep <nguyennhutuanhiep@gmail.com>
2018-06-16 16:49:12 +0700

    print output

---
+++ src/main/java/com/higgsup/intern/w01/sec03/Exercise14.java
@@ -0,0 +1,30 @@
+package com.higgsup.intern.w01.sec03;
+
+public class Exercise14 {
+    public static void main(String[] args) {
+        int f = 0, g = 1;
+        for (int i = 0; i <= 15; i++) {
+            System.out.println(f);
+            f = f + g;
+            g = f - g;
+        }
+    }
+}
+    /* print out
+        0
+        1
+        1
+        2
+        3
+        5
+        8
+        13
+        21
+        34
+        55
+        89
+        144
+        233
+        377
+        610
+     */
