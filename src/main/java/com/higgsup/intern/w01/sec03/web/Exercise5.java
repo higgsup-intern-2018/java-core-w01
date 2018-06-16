61e89ff36a0557b78205ff4ced9bd6c509e6eb41
tuanhiep <nguyennhutuanhiep@gmail.com>
2018-06-16 17:13:27 +0700

    Infinite for-loop

---
+++ src/main/java/com/higgsup/intern/w01/sec03/web/Exercise5.java
@@ -0,0 +1,11 @@
+package com.higgsup.intern.w01.sec03.web;
+
+public class Exercise5
+{
+    public static void main(String[] args) {
+        for(;;)
+        {
+            System.out.println("Infinite loop");
+        }
+    }
+}
