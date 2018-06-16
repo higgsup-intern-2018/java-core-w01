4e2145a77b6b643740484c2255c6c6aa871626c2
tuanhiep <nguyennhutuanhiep@gmail.com>
2018-06-16 14:36:12 +0700

    statements do

---
+++ src/main/java/com/higgsup/intern/w01/sec02/web/Exercise2.java
@@ -0,0 +1,9 @@
+package com.higgsup.intern.w01.sec02.web;
+
+public class Exercise2
+{
+    public static void main(String[] args) {
+       // boolean isA = (90 <= grade <= 100)
+        // syntax error, must be 90 <= grade && grade <= 100)
+    }
+}
