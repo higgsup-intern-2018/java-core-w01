c57760f984e7430b413cba589c3a5c849c5e734d
tuanhiep <nguyennhutuanhiep@gmail.com>
2018-06-16 17:16:34 +0700

    while loop problem

---
+++ src/main/java/com/higgsup/intern/w01/sec03/web/Exercise6.java
@@ -0,0 +1,13 @@
+package com.higgsup.intern.w01.sec03.web;
+
+public class Exercise6
+{
+    public static void main(String[] args) {
+        boolean done = false;
+        while(done = false)
+        {
+            System.out.println("Hello?");
+        }
+        //problem: use assign operator = => code inside loop never execute
+    }
+}
