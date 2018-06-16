74f330309c0ac4f01b95453974cc4dff4ba2d1fd
tuanhiep <nguyennhutuanhiep@gmail.com>
2018-06-16 14:59:09 +0700

    code fragment print

---
+++ src/main/java/com/higgsup/intern/w01/sec02/web/Exercise5.java
@@ -0,0 +1,11 @@
+package com.higgsup.intern.w01.sec02.web;
+
+public class Exercise5
+{
+    public static void main(String[] args) {
+        double x = (double) (3 / 5);
+        System.out.println(x);
+
+        //system print 0.0 (division put in parentheses done before the cast)
+    }
+}
