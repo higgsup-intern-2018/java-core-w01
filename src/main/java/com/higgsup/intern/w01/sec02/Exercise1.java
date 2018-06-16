2126aef6496653066577179c9e338b4776488542
tuanhiep <nguyennhutuanhiep@gmail.com>
2018-06-16 10:26:58 +0700

    Following sequence of statements do

--- src/main/java/com/higgsup/intern/w01/sec02/Exercise1.java
+++ src/main/java/com/higgsup/intern/w01/sec02/Exercise1.java
@@ -1,4 +1,11 @@
 package com.higgsup.intern.w01.sec02;
 
-public class Exercise1 {
+public class Exercise1
+{
+    public static void main(String[] args) {
+        int a = 10, b = 8;
+        int t = a;      //assignment statements, change value of t equal to value of a
+        b = t;          //assignment statements, change value of b equal to value of t
+        a = b;          //assignment statements, change value of a equal to value of b
+    }
 }
