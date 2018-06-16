910d1a09e2037856339908ce4a12d068e40c5731
tuanhiep <nguyennhutuanhiep@gmail.com>
2018-06-16 16:13:31 +0700

    all three equal or not

--- src/main/java/com/higgsup/intern/w01/sec03/Exercise1.java
+++ src/main/java/com/higgsup/intern/w01/sec03/Exercise1.java
@@ -1,4 +1,16 @@
 package com.higgsup.intern.w01.sec03;
 
-public class Exercise1 {
+public class Exercise1
+{
+    public static void main(String[] args) {
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+        int c = Integer.parseInt(args[2]);
+
+        if (a == b && a == c) {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
+    }
 }
