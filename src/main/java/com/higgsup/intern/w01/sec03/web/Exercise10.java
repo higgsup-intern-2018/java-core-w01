d93bcbbf4957947990828d94c7094372cf25a90a
tuanhiep <nguyennhutuanhiep@gmail.com>
2018-06-16 17:34:37 +0700

    Change character for loop

---
+++ src/main/java/com/higgsup/intern/w01/sec03/web/Exercise10.java
@@ -0,0 +1,12 @@
+package com.higgsup.intern.w01.sec03.web;
+
+public class Exercise10
+{
+    public static void main(String[] args) {
+        int i, n = 20;
+        for (i = 0; i < n; i--)
+            System.out.print("x");
+    }
+
+    //Change/add one character: change i-- to n--
+}
