d77051b1a2ee0df5e7a04a87fb9904c2b54e5e78
tuanhiep <nguyennhutuanhiep@gmail.com>
2018-06-16 15:10:09 +0700

    try to write expression

---
+++ src/main/java/com/higgsup/intern/w01/sec02/web/Exercise15.java
@@ -0,0 +1,9 @@
+package com.higgsup.intern.w01.sec02.web;
+
+public class Exercise15
+{
+    public static void main(String[] args) {
+        double x;
+        System.out.println(x);      //error: variable x might not have been initialized
+    }
+}
