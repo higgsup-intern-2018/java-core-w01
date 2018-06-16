064e999dbfe23bb3b2092edcb7ee7405534c92e7
tuanhiep <nguyennhutuanhiep@gmail.com>
2018-06-16 17:36:37 +0700

    code fragment do

---
+++ src/main/java/com/higgsup/intern/w01/sec03/web/Exercise11.java
@@ -0,0 +1,12 @@
+package com.higgsup.intern.w01.sec03.web;
+
+public class Exercise11
+{
+    public static void main(String[] args) {
+        int x = -3;
+        if (x > 0);
+            System.out.println("positive");
+    }
+
+    //print out positive
+}
