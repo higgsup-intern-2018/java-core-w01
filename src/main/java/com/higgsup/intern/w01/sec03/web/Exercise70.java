f8ca775e593028c18bc852cac42e53238edde745
tuanhiep <nguyennhutuanhiep@gmail.com>
2018-06-16 18:11:49 +0700

    Code fragment compile

---
+++ src/main/java/com/higgsup/intern/w01/sec03/web/Exercise70.java
@@ -0,0 +1,12 @@
+package com.higgsup.intern.w01.sec03.web;
+
+public class Exercise70
+{
+    public static void main(String[] args) {
+        int a = 10, b = 18;
+        if (a = b) System.out.println("equal");
+        else       System.out.println("not equal");
+    }
+
+    //error in conditional of if: using assignment operator => compile error
+}
