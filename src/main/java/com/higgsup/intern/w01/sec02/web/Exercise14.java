d1a8d44ed3c8d2058aeb7d4bc1c508d9745f24af
tuanhiep <nguyennhutuanhiep@gmail.com>
2018-06-16 15:08:24 +0700

    try to write following expression

---
+++ src/main/java/com/higgsup/intern/w01/sec02/web/Exercise14.java
@@ -0,0 +1,9 @@
+package com.higgsup.intern.w01.sec02.web;
+
+public class Exercise14
+{
+    public static void main(String[] args) {
+        int a = 27 * "three";       //error: bad operand types for binary operator *
+        System.out.println(a);
+    }
+}
