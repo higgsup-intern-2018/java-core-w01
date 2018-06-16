3ed234ba259eac2bd81f09469633c029ca6c3375
Thieu Tu <tu.thieuthanh@gmail.com>
2018-06-16 16:08:08 +0700

    Complete sec02-Web-Exercise 06!

---
+++ src/main/java/com/higgsup/intern/w01/sec02/Exercise06.java
@@ -0,0 +1,10 @@
+package com.higgsup.intern.w01.sec02;
+
+public class Exercise06 {
+    public static void main(String[] args) {
+        System.out.println(10/3);
+    /*Kết quả in ra là: 3, vì khi ta viết 10/3 trình biên dịch đã ngầm hiểu 10 và 3 đều thuộc Integer và đây là phép
+        chia số nguyên, nên kết quả là 1 số nguyên
+    Nếu muốn kết quả là 3.3333 thì ta có thể viết là 10.0/3 hoặc 10/3.0.*/
+    }
+}
