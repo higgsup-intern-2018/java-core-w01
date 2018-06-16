622df41dedecc60d61697c9f0a2e08966b188147
tuanhiep <nguyennhutuanhiep@gmail.com>
2018-06-16 10:11:18 +0700

    10 / 3 = 3

---
+++ src/main/java/com/higgsup/intern/w01/sec02/Exercise6.java
@@ -0,0 +1,10 @@
+package com.higgsup.intern.w01.sec02;
+
+public class Exercise6
+{
+    public static void main(String[] args) {
+        System.out.println(10/3);
+
+        //Result is 3 since int / int will get int result
+    }
+}
