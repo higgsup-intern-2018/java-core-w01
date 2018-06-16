22e77729586d4e66cb2dcf606ade093eaa52cd1f
Thieu Tu <tu.thieuthanh@gmail.com>
2018-06-16 18:34:03 +0700

    Complete sec02-Web-Exercise 14!

---
+++ src/main/java/com/higgsup/intern/w01/sec02/web/Exercise14.java
@@ -0,0 +1,9 @@
+package com.higgsup.intern.w01.sec02.web;
+
+public class Exercise14 {
+    public static void main(String[] args) {
+        //int a = 27 * "three";
+        // Toán tử * không thể dùng với string
+
+    }
+}
