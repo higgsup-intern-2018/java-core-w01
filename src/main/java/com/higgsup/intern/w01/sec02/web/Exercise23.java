c786fc63a64227825f5b53ab009559182fc429cb
tuanhiep <nguyennhutuanhiep@gmail.com>
2018-06-16 15:39:30 +0700

    temperature conversion code fragment

---
+++ src/main/java/com/higgsup/intern/w01/sec02/web/Exercise23.java
@@ -0,0 +1,9 @@
+package com.higgsup.intern.w01.sec02.web;
+
+public class Exercise23
+{
+    public static void main(String[] args) {
+        double F;
+        double C = (F - 32) * (5/9);        //F might not have been initialized
+    }
+}
