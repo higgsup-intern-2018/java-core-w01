27b282d815b35bf6de4c3dbc5763f3ab3d7a7004
tuanhiep <nguyennhutuanhiep@gmail.com>
2018-06-16 18:13:41 +0700

    Code fragment

---
+++ src/main/java/com/higgsup/intern/w01/sec03/web/Exercise71.java
@@ -0,0 +1,12 @@
+package com.higgsup.intern.w01.sec03.web;
+
+public class Exercise71
+{
+    public static void main(String[] args) {
+        boolean a = false;
+        if (a = true) System.out.println("yes");
+        else          System.out.println("no");
+    }
+
+    //using assignment operator in if condition => always print true
+}
