f35e8c375d570a7e100f4f36fc5433bda270425f
tuanhiep <nguyennhutuanhiep@gmail.com>
2018-06-16 16:56:58 +0700

    Rolling die

--- src/main/java/com/higgsup/intern/w01/sec03/web/Exercise1.java
+++ src/main/java/com/higgsup/intern/w01/sec03/web/Exercise1.java
@@ -1,4 +1,9 @@
 package com.higgsup.intern.w01.sec03.web;
 
-public class Exercise1 {
+public class Exercise1
+{
+    public static void main(String[] args) {
+        int rollDie = (int)(Math.random() * 6 + 1);
+        System.out.println("Rolling die result: " + rollDie);
+    }
 }
