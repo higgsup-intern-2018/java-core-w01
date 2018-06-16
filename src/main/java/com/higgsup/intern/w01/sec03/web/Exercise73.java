cdf2e0c8029afb663d165a6e64dedf7a9ee73803
tuanhiep <nguyennhutuanhiep@gmail.com>
2018-06-16 18:17:33 +0700

    Code fragment do

---
+++ src/main/java/com/higgsup/intern/w01/sec03/web/Exercise73.java
@@ -0,0 +1,12 @@
+package com.higgsup.intern.w01.sec03.web;
+
+public class Exercise73
+{
+    public static void main(String[] args) {
+        for (int x = 0; x < 100; x += 0.5) {
+            System.out.println(x);
+        }
+    }
+
+    //infinite loop printing 0
+}
