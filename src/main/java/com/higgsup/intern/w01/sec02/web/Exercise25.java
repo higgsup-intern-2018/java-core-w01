b507bbbdae8020709e1e2e6aa07793408e7fff7c
tuanhiep <nguyennhutuanhiep@gmail.com>
2018-06-16 15:42:43 +0700

    Legal statements

---
+++ src/main/java/com/higgsup/intern/w01/sec02/web/Exercise25.java
@@ -0,0 +1,12 @@
+package com.higgsup.intern.w01.sec02.web;
+
+public class Exercise25
+{
+    public static void main(String[] args)
+    {
+        //boolean b = 1;            //illegal (incompatible types)
+        //boolean b = true;         //legal
+        //boolean b = "true";       //illegal (incompatible types)
+        //boolean b = True;         //illegal (cannot resolve symbol)
+    }
+}
