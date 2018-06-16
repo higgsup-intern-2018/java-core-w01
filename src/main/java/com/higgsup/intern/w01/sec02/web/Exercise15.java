f57dd6e78475fc37b51759beb22930ae30762751
Thieu Tu <tu.thieuthanh@gmail.com>
2018-06-16 18:37:00 +0700

    Complete sec02-Web-Exercise 15!

--- src/main/java/com/higgsup/intern/w01/sec02/web/Exercise15.java
+++ src/main/java/com/higgsup/intern/w01/sec02/web/Exercise15.java
@@ -3,6 +3,7 @@ package com.higgsup.intern.w01.sec02.web;
 public class Exercise15 {
     public static void main(String[] args) {
         double x;
-        System.out.println(x);// Lỗi do x chưa được khởi tạo giá trị
+        //System.out.println(x);Lỗi do x chưa được khởi tạo giá trị
+
     }
 }
