c74aaa4357ac9e32603e8ff7b83b5ec4c7d1d4ec
tuanhiep <nguyennhutuanhiep@gmail.com>
2018-06-16 10:30:11 +0700

    Simplify expression

---
+++ src/main/java/com/higgsup/intern/w01/sec02/Exercise4.java
@@ -0,0 +1,7 @@
+package com.higgsup.intern.w01.sec02;
+
+public class Exercise4
+{
+    //(!(a < b) && !(a > b)) expression means a is neither greater than nor smaller than b
+    // => a equals b => simplify: a == b
+}
