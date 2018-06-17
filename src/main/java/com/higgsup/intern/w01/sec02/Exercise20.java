b20051fad6a6d62d1674d3857ff055bde6272ba6
tuanhiep <nguyennhutuanhiep@gmail.com>
2018-06-17 15:55:03 +0700

    Sum of two random integer between 1 and 6

--- src/main/java/com/higgsup/intern/w01/sec02/Exercise20.java
+++ src/main/java/com/higgsup/intern/w01/sec02/Exercise20.java
@@ -1,13 +1,11 @@
 package com.higgsup.intern.w01.sec02;
 
-import java.util.Random;
 
 public class Exercise20
 {
     public static void main(String[] args) {
-        Random rd = new Random();
-        int firstRandom = rd.nextInt(6) + 1;
-        int secondRandom = rd.nextInt(6) + 1;
+        int firstRandom = (int)(Math.random() * 6) + 1;
+        int secondRandom = (int)(Math.random() * 6) + 1;
 
         int sum = firstRandom + secondRandom;
         System.out.println("Sum of two random integers between 1 and 6: " + sum);
