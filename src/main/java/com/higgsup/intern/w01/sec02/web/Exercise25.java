c2665a45812f65e11e1331ebad301f1f3a2c2aec
Thieu Tu <tu.thieuthanh@gmail.com>
2018-06-17 11:21:17 +0700

    Complete sec02-Web-Exercise 25!

---
+++ src/main/java/com/higgsup/intern/w01/sec02/web/Exercise25.java
@@ -0,0 +1,11 @@
+package com.higgsup.intern.w01.sec02.web;
+
+public class Exercise25 {
+    public static void main(String[] args) {
+        //boolean b = 1;
+        boolean b = true;  //đúng vì boolean chỉ chứa giá trị true hoặc false.
+        //boolean b = "true";
+        //boolean b = True;
+        System.out.println(b);
+    }
+}
