7818317e1ddb90bb168bf0b49501f392a40276f7
tuanhiep <nguyennhutuanhiep@gmail.com>
2018-06-17 20:06:13 +0700

    Swap value

--- src/main/java/com/higgsup/intern/w01/sec02/web/Exercise1.java
+++ src/main/java/com/higgsup/intern/w01/sec02/web/Exercise1.java
@@ -1,4 +1,17 @@
 package com.higgsup.intern.w01.sec02.web;
 
-public class Exercise1 {
+public class Exercise1
+{
+    public static void main(String[] args)
+    {
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+        System.out.println("a: " + a + ", b: " + b);
+        a = a - b;
+        System.out.println("a: " + a + ", b: " + b);
+        b = a + b;
+        System.out.println("a: " + a + ", b: " + b);
+        a = b - a;
+        System.out.println("a: " + a + ", b: " + b);
+    }
 }
