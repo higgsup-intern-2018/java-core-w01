d829cbc6596a9bf04c5b03b918bfa95259efa73f
Thieu Tu <tu.thieuthanh@gmail.com>
2018-06-17 21:34:58 +0700

    Complete sec01-Web-Exercise 05!

---
+++ src/main/java/com/higgsup/intern/w01/sec01/web/Exercise05.java
@@ -0,0 +1,9 @@
+package com.higgsup.intern.w01.sec01.web;
+
+public class Exercise05 {
+    //public static void main()
+    public static void main(String[] args) {
+        System.out.println("Doesn't execute");
+    }
+}
+//Bạn quên String[] args.
