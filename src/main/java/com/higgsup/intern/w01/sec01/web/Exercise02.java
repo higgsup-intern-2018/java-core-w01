44ab81509b2198a4b10eaed87d967b2dcbaebb2e
Thieu Tu <tu.thieuthanh@gmail.com>
2018-06-17 11:35:29 +0700

    Complete sec01-Web-Exercise 02!

---
+++ src/main/java/com/higgsup/intern/w01/sec01/web/Exercise02.java
@@ -0,0 +1,15 @@
+package com.higgsup.intern.w01.sec01.web;
+
+public class Exercise02 {
+    public static void main (String [] args) {
+        System.out.println("Hello, World");
+    }
+    /*
+    * Nếu thiếu:
+    * a. main: Chương trình sẽ báo lỗi do class Exercise02 không có hàm main, cái này bắt buộc ở bất kỳ class nào.
+    * b. String: Chương trình sẽ báo lỗi do khai báo phương thức ko có giá trị trả về.
+    * c. HelloWorld: Chương trình vẫn chạy bình thường, tuy nhiên sẽ không in gì ra màn hình cả.
+    * d. System.out: Chương trình sẽ báo lỗi do không tìm thấy phương thức println.
+    * e. println: Chương trình sẽ báo lỗi do không có phương thức.
+    */
+}
