d5f4227c947cf8a79a64c00776040154b553b73e
tuanhiep <nguyennhutuanhiep@gmail.com>
2018-06-17 10:36:27 +0700

    Hello lines

---
+++ src/main/java/com/higgsup/intern/w01/sec03/Exercise8.java
@@ -0,0 +1,26 @@
+package com.higgsup.intern.w01.sec03;
+
+public class Exercise8
+{
+    public static void main(String[] args) {
+        int numberLines = Integer.parseInt(args[0]);
+        String suffix;
+
+        for(int i = 1; i <= numberLines; i++)
+        {
+            if(i % 10 == 1 && i != 11)
+            {
+                suffix = "st";
+            }else if(i % 10 == 2 && i != 12)
+            {
+                suffix = "nd";
+            }else if(i % 10 == 3 && i != 13)
+            {
+                suffix = "rd";
+            }else {
+                suffix = "th";
+            }
+            System.out.println(i+suffix + " hello.");
+        }
+    }
+}
