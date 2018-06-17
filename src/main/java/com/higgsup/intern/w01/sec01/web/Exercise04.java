96bb6dbc70c0f8fcdd74da725f69e722c46fba88
Thieu Tu <tu.thieuthanh@gmail.com>
2018-06-17 21:31:35 +0700

    Complete sec01-Web-Exercise 04!

--- src/main/java/com/higgsup/intern/w01/sec01/web/Exercise04.java
+++ src/main/java/com/higgsup/intern/w01/sec01/web/Exercise04.java
@@ -3,7 +3,8 @@ package com.higgsup.intern.w01.sec01.web;
 public class Exercise04 {
     public static void main(String[] args) {
         System.out.println("HelloWorld");
-        /*a. main: Khi ta viết sai chính tả thì trình duyệt sẽ ko chạy được do thiếu hàm main
+        /*
+         a. main: Khi ta viết sai chính tả thì trình duyệt sẽ ko chạy được do thiếu hàm main
          b. String: Khi ta viết sai chính tả thì trình duyệt sẽ báo lỗi rằng không thể giải quyết loại "Sring"
          c. HelloWorld: Khi ta viết sai chính tả thì kết quả sẽ hiển thị đúng như cái ta viết
          d. System.out: Khi ta viết sai chính tả thì trình duyệt sẽ báo lỗi rằng không thể giải quyết loại "Sstem""
