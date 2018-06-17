c3b47d804e22dad32766393edb964ab9aa3e0215
DucThanh6111996 <Hoahongden123>
2018-06-17 11:15:37 +0700

    useThree

---
+++ src/main/java/com/higgsup/intern/w01/sec01/UseThree.java
@@ -0,0 +1,12 @@
+package com.higgsup.intern.w01.sec01;
+
+public class UseThree {
+    public static void main(String[] args) {
+        System.out.println("Hi");
+        System.out.println(args[3]);
+        System.out.println(args[2]);
+        System.out.println(args[1]);
+        System.out.println(args[0]);
+
+    }
+}
