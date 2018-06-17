4eb5fe69c0d1b5e6a4618d3145268d8bb70d9066
tuanhiep <nguyennhutuanhiep@gmail.com>
2018-06-17 17:04:22 +0700

    Temperature conversion

--- src/main/java/com/higgsup/intern/w01/sec02/web/Exercise23.java
+++ src/main/java/com/higgsup/intern/w01/sec02/web/Exercise23.java
@@ -3,7 +3,8 @@ package com.higgsup.intern.w01.sec02.web;
 public class Exercise23
 {
     public static void main(String[] args) {
-        double F;
-        double C = (F - 32) * (5/9);        //F might not have been initialized
+        double F = 58;
+        double C = (F - 32) * (5/9);        //Result always is 0.0 since 5/9 is 0
+        System.out.println(C);
     }
 }
