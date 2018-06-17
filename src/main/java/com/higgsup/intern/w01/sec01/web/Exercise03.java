c9adbadfbdc22fb1e3fceb6c04dbd719f253c0ff
Thieu Tu <tu.thieuthanh@gmail.com>
2018-06-17 21:17:08 +0700

    Complete sec01-Web-Exercise 03!

---
+++ src/main/java/com/higgsup/intern/w01/sec01/web/Exercise03.java
@@ -0,0 +1,15 @@
+package com.higgsup.intern.w01.sec01.web;
+
+public class Exercise03 {
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+        /* the ; :Chương trình sẽ báo lỗi rằng thiếu dấu ;
+        the first ": Chương trình sẽ báo lỗi
+        the second ": Chương trình sẽ báo lỗi
+        the first { : Chương trình sẽ báo lỗi cần thêm dấu {
+        the second {: Chương trình sẽ báo lỗi cần thêm dấu {
+        the first }: Chương trình sẽ báo lỗi cần thêm dấu }
+        the second }: Chương trình sẽ báo lỗi cần thêm dấu }
+        */
+    }
+}
