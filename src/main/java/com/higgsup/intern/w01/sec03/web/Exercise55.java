6be1a4aabbd60cfb610fcffdf88a37973a853dbb
tuanhiep <nguyennhutuanhiep@gmail.com>
2018-06-17 09:26:30 +0700

    Season corresponding to month

---
+++ src/main/java/com/higgsup/intern/w01/sec03/web/Exercise55.java
@@ -0,0 +1,53 @@
+package com.higgsup.intern.w01.sec03.web;
+
+public class Exercise55
+{
+    public static void main(String[] args)
+    {
+        int day = Integer.parseInt(args[0]);
+        int month = Integer.parseInt(args[1]);
+
+        switch(month) {
+            case 3:
+                if (day > 20) {
+                    System.out.println("Season: Spring");
+                } else {
+                    System.out.println("Season: Winter");
+                }
+                break;
+            case 4: case 5:
+                System.out.println("Season: Spring");
+                break;
+            case 6:
+                if(day < 21) {
+                    System.out.println("Season: Spring");
+                }else {
+                        System.out.println("Season: Summer");
+                }
+                break;
+            case 7: case 8:
+                System.out.println("Season: Summer");
+                break;
+            case 9:
+                if(day < 23) {
+                    System.out.println("Season: Summer");
+                }else{
+                    System.out.println("Season: Fall");
+                }
+                break;
+            case 10: case 11:
+                System.out.println("Season: Fall");
+                break;
+            case 12:
+                if(day < 21) {
+                    System.out.println("Season: Fall");
+                }else{
+                    System.out.println("Season: Winter");
+                }
+                break;
+            case 1: case 2:
+                System.out.println("Season: Winter");
+                break;
+        }
+    }
+}
