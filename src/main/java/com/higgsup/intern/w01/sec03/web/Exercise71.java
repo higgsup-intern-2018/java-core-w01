98edf91218bf762846ae44158afcbf9c18335b2d
Thieu Tu <tu.thieuthanh@gmail.com>
2018-06-18 22:41:38 +0700

    Complete sec03-Web-Exercise 71!

---
+++ src/main/java/com/higgsup/intern/w01/sec03/web/Exercise71.java
@@ -0,0 +1,10 @@
+package com.higgsup.intern.w01.sec03.web;
+
+public class Exercise71 {
+    public static void main(String[] args) {
+        boolean a = false;
+        if (a == true) System.out.println("yes"); //Chuyển thành ==
+        else          System.out.println("no");
+    }
+}
+//Giải thích: In ra yes nếu a == true, ngược lại in ra no
