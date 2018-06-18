9a7373f858c21664404fda0ab4f0e07afc9579c4
Thieu Tu <tu.thieuthanh@gmail.com>
2018-06-18 13:36:40 +0700

    Complete sec03-Web-Exercise 11!

---
+++ src/main/java/com/higgsup/intern/w01/sec03/web/Exercise11.java
@@ -0,0 +1,10 @@
+package com.higgsup.intern.w01.sec03.web;
+
+public class Exercise11 {
+    public static void main(String[] args) {
+        int x = -9;
+        if (x > 0);
+        System.out.println("positive"); // Thường xuyên in "positive" với mọi giá trị của x, do lệnh line 4 không thuộc
+        // trong mệnh đề if
+    }
+}
