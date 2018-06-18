ac27ac956781b3b52fb56a2dc802655161eaf1ca
Thieu Tu <tu.thieuthanh@gmail.com>
2018-06-18 22:38:47 +0700

    Complete sec03-Web-Exercise 70!

---
+++ src/main/java/com/higgsup/intern/w01/sec03/web/Exercise70.java
@@ -0,0 +1,9 @@
+package com.higgsup.intern.w01.sec03.web;
+//Will the following code fragment compile? If so, what will it do?
+public class Exercise70 {
+    public static void main(String[] args) {
+        int a = 10, b = 18;
+        if (a == b) System.out.println("equal"); //Chuyển = thành ==
+        else       System.out.println("not equal");
+    }
+}
