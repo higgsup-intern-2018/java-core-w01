0edcd48d12fe1e2723ac5c2c4cbd38e960946249
DucThanh6111996 <Hoahongden123>
2018-06-18 21:39:47 +0700

    bai24

---
+++ src/main/java/com/higgsup/intern/w01/sec02/web/Exercise24.java
@@ -0,0 +1,9 @@
+package com.higgsup.intern.w01.sec02.web;
+
+public class Exercise24 {
+    public static void main(String[] args) {
+        double a=20;
+        double b = Math.pow(a,2);
+        System.out.println(b);
+    }
+}
