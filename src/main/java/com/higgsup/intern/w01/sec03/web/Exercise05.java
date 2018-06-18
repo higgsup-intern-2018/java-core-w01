34e4c4ad91ba7ddc14df3191f55c2fd5ee219dfe
Thieu Tu <tu.thieuthanh@gmail.com>
2018-06-18 11:38:41 +0700

    Complete sec03-Web-Exercise 05!

---
+++ src/main/java/com/higgsup/intern/w01/sec03/web/Exercise05.java
@@ -0,0 +1,10 @@
+package com.higgsup.intern.w01.sec03.web;
+//How can I create in an infinite loop with a for loop?
+public class Exercise05{
+    public static void main(String[] args) {
+        for (;;){
+            System.out.println("loop");
+        }
+    }
+
+}
