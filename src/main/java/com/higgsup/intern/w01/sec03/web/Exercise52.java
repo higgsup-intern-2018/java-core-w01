eca98da69e5e95981e81d61299cea1582c3bd265
Thieu Tu <tu.thieuthanh@gmail.com>
2018-06-18 23:20:36 +0700

    Complete sec03-Web-Exercise 52!

---
+++ src/main/java/com/higgsup/intern/w01/sec03/web/Exercise52.java
@@ -0,0 +1,15 @@
+package com.higgsup.intern.w01.sec03.web;
+
+public class Exercise52 {
+    public static void main(String[] args) {
+        int n = Integer.parseInt(args[0]);
+
+        for (int i = -n; i <= n; i++) {
+            for (int j = -n; j <= n; j++) {
+                if (Math.abs(i) + Math.abs(j) <= n) System.out.print("* ");
+                else                                System.out.print(". ");
+            }
+            System.out.println();
+        }
+    }
+}
