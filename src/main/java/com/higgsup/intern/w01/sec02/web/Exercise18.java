d0f4a8d2e8dd4719fc55efa3bfa9f6d4d6bca9d3
DucThanh6111996 <Hoahongden123>
2018-06-18 21:38:13 +0700

    bai18

---
+++ src/main/java/com/higgsup/intern/w01/sec02/web/Exercise18.java
@@ -0,0 +1,13 @@
+package com.higgsup.intern.w01.sec02.web;
+
+public class Exercise18 {
+    public static void main(String[] args) {
+        int x = Integer.parseInt(args[0]);
+        int y = Integer.parseInt(args[1]);
+        int z = Integer.parseInt(args[2]);
+
+        boolean b = (x==y) && (x==z);
+        System.out.println(b);
+
+    }
+}
