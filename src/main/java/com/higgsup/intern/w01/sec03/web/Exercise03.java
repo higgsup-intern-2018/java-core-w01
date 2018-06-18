9def3ee11dcdb2f99de47ec84a0a4c92d0ee74a5
Thieu Tu <tu.thieuthanh@gmail.com>
2018-06-18 11:34:19 +0700

    Complete sec03-Web-Exercise 03!

---
+++ src/main/java/com/higgsup/intern/w01/sec03/web/Exercise03.java
@@ -0,0 +1,19 @@
+package com.higgsup.intern.w01.sec03.web;
+
+//Write a program that takes five integer command-line arguments and prints the median (the third largest one).
+public class Exercise03 {
+    public static void main(String[] args) {
+     String tempt = args[0];
+
+        for (int i = 0; i < args.length - 1; i++){
+            for(int j = i + 1 ; j< args.length; j++){
+                if (Integer.parseInt(args[i]) > Integer.parseInt(args[j])){
+                    tempt = args[i];
+                    args[i] = args[j];
+                    args[j] = tempt;
+                }
+            }
+        }
+        System.out.println("The median: " + args[2]);
+    }
+}
