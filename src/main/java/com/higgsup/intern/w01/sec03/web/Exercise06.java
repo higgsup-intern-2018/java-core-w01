09c28dd2b6de14ba2213e2f0b3b8403b56563229
Thieu Tu <tu.thieuthanh@gmail.com>
2018-06-18 11:44:36 +0700

    Complete sec03-Web-Exercise 06!

---
+++ src/main/java/com/higgsup/intern/w01/sec03/web/Exercise06.java
@@ -0,0 +1,11 @@
+package com.higgsup.intern.w01.sec03.web;
+
+public class Exercise06 {
+    public static void main(String[] args) {
+
+        boolean done = false;
+        while (done = false) { // Chuyển thành ==. Nhưng nếu dùng như vậy thì vòng lặp không được thực thi. Ta nên sửa thành done != done
+
+        }
+    }
+}
