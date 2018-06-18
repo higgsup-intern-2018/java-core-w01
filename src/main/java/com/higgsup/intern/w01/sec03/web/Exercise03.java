2bb45825b683fe83f4563f49e279b1ae35f18b94
Thieu Tu <tu.thieuthanh@gmail.com>
2018-06-18 11:36:35 +0700

    Delete the variable initialization: "tempt"

--- src/main/java/com/higgsup/intern/w01/sec03/web/Exercise03.java
+++ src/main/java/com/higgsup/intern/w01/sec03/web/Exercise03.java
@@ -3,7 +3,7 @@ package com.higgsup.intern.w01.sec03.web;
 //Write a program that takes five integer command-line arguments and prints the median (the third largest one).
 public class Exercise03 {
     public static void main(String[] args) {
-     String tempt = args[0];
+     String tempt;
 
         for (int i = 0; i < args.length - 1; i++){
             for(int j = i + 1 ; j< args.length; j++){
