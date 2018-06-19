9f7f24babc28b85ea0678210e506bf540af25b06
DucThanh6111996 <Hoahongden123>
2018-06-19 10:20:40 +0700

    Exercise01

--- src/main/java/com/higgsup/intern/w01/sec03/web/Exercise01.java
+++ src/main/java/com/higgsup/intern/w01/sec03/web/Exercise01.java
@@ -1,4 +1,9 @@
 package com.higgsup.intern.w01.sec03.web;
 
 public class Exercise01 {
+    public static void main(String[] args) {
+        int side=6;
+        int roll =(int)(Math.random() *side)+1;
+        System.out.println(roll);
+    }
 }
