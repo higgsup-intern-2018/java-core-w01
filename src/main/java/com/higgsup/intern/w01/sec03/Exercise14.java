c099de133149a51dcc4a3482db93c8099f393f95
DucThanh6111996 <Hoahongden123>
2018-06-19 09:39:42 +0700

    exercise14

---
+++ src/main/java/com/higgsup/intern/w01/sec03/Exercise14.java
@@ -0,0 +1,12 @@
+package com.higgsup.intern.w01.sec03;
+
+public class Exercise14 {
+    public static void main(String[] args) {
+        int f = 0, g = 1;
+        for (int i = 0; i <= 15; i++) {
+            System.out.println(f);
+            f = f + g;
+            g = f - g;
+        }
+    }
+}
