dd21f8dacabf0d6550531358f33e111e818a45f3
Thieu Tu <tu.thieuthanh@gmail.com>
2018-06-19 17:31:03 +0700

    Complete sec03-Web-Exercise 44!

--- src/main/java/com/higgsup/intern/w01/sec03/web/Exercise44.java
+++ src/main/java/com/higgsup/intern/w01/sec03/web/Exercise44.java
@@ -3,9 +3,13 @@ package com.higgsup.intern.w01.sec03.web;
 public class Exercise44 {
     public static void main(String[] args) {
         int i = 10;
-        i = i++; // i=11
-        i = ++i; // i=12
-        i = i++ + ++i; //i=12+12=24
+        System.out.println(i); // i = 10
+        i = i++;
+        System.out.println(i); //i =10
+        i = ++i;
+        System.out.println(i); //i=11
+
+        i = i++ + ++i;
         System.out.println(i); //i=24
     }
 }
