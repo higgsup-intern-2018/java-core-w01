5687ff16d4b6bc5852a5f074c6ab877224dea6e7
DucThanh6111996 <Hoahongden123>
2018-06-19 09:39:05 +0700

    Exercise01_sec3

--- src/main/java/com/higgsup/intern/w01/sec03/Exercise01.java
+++ src/main/java/com/higgsup/intern/w01/sec03/Exercise01.java
@@ -1,4 +1,20 @@
 package com.higgsup.intern.w01.sec03;
-
+import java.util.*;
 public class Exercise01 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a,b,c;
+        System.out.println("Nhập a= ");
+        a= sc.nextInt();
+        System.out.println("Nhập b=");
+        b=sc.nextInt();
+        System.out.println("Nhập c= ");
+        c=sc.nextInt();
+
+        if(a==b && a==c){
+            System.out.println("Tất cả bằng nhau");
+        }else{
+            System.out.println("Tất cả không bằng nhau");
+        }
+    }
 }
