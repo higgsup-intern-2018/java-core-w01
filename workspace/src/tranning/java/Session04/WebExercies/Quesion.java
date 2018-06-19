819a2abfcd9e6c3e5777349f32263aaabbcef789
NgThuy <ngothuy05111998@gmail.com>
2018-06-19 15:53:20 +0700

    revert master branch

--- workspace/src/tranning/java/Session04/WebExercies/Quesion.java
+++
@@ -1,9 +0,0 @@
-package tranning.java.Session04.WebExercies;
-
-public class Quesion {
-    /*
-    * 31.What happens when you try to compile a program with the following statement?
-    * int[] a = new int[-17];
-    *Exception : java.lang.NegativeArraySizeException
-    * */
-}
