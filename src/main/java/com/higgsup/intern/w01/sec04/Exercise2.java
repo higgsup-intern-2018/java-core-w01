5748974b321d040868e7beb58aeacad723f3ed90
tuanhiep <nguyennhutuanhiep@gmail.com>
2018-06-20 16:51:42 +0700

    What happens?

---
+++ src/main/java/com/higgsup/intern/w01/sec04/Exercise2.java
@@ -0,0 +1,13 @@
+package com.higgsup.intern.w01.sec04;
+
+public class Exercise2
+{
+    public static void main(String[] args)
+    {
+        int n = 1000;
+        int[] a = new int[n*n*n*n];
+    }
+
+    //compile normal, throw exception NegativeArraySizeException when run
+
+}
