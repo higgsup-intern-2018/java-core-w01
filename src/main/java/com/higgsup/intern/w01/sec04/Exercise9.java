ce1c702323dc5228e2d01a3739db38be289e59c3
tuanhiep <nguyennhutuanhiep@gmail.com>
2018-06-20 16:58:06 +0700

    Following code fragment print

---
+++ src/main/java/com/higgsup/intern/w01/sec04/Exercise9.java
@@ -0,0 +1,12 @@
+package com.higgsup.intern.w01.sec04;
+
+public class Exercise9
+{
+    public static void main(String[] args) {
+        int[] a = { 1, 2, 3 };
+        int[] b = { 1, 2, 3 };
+        System.out.println(a == b);
+
+        //print false (a and b has different memory address)
+    }
+}
