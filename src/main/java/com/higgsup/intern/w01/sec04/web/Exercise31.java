e2a694f88debbb82a8b25073549335e7d00ce776
tuanhiep <nguyennhutuanhiep@gmail.com>
2018-06-20 17:07:27 +0700

    What happens

---
+++ src/main/java/com/higgsup/intern/w01/sec04/web/Exercise31.java
@@ -0,0 +1,10 @@
+package com.higgsup.intern.w01.sec04.web;
+
+public class Exercise31
+{
+    public static void main(String[] args) {
+        int i[] = new int[-17];
+    }
+
+    //compile normal, throw NegativeArraySizeException when run
+}
