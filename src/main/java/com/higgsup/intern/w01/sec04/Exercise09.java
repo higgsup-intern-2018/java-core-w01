031416e132468fc1d6834fd70beacb55ba0fb20b
Thieu Tu <tu.thieuthanh@gmail.com>
2018-06-20 21:23:13 +0700

    Complete sec04-Exercise 09!

---
+++ src/main/java/com/higgsup/intern/w01/sec04/Exercise09.java
@@ -0,0 +1,9 @@
+package com.higgsup.intern.w01.sec04;
+
+public class Exercise09 {
+    public static void main(String[] args) {
+        int[] a = { 1, 2, 3 };
+        int[] b = { 1, 2, 3 };
+        System.out.println(a == b);  //In False do a và b không cùng địa chỉ bộ nhớ
+    }
+}
