c656e548da5625957e61eed173a0d3cbb04a74a1
Thieu Tu <tu.thieuthanh@gmail.com>
2018-06-20 21:26:01 +0700

    Complete sec04-Exercise 11!

---
+++ src/main/java/com/higgsup/intern/w01/sec04/Exercise11.java
@@ -0,0 +1,8 @@
+package com.higgsup.intern.w01.sec04;
+//Write a program HowMany.java that takes a variable number of command-line arguments
+// and prints how many there are.
+public class Exercise11 {
+    public static void main(String[] args) {
+        System.out.println("The number of variables: " + args.length);
+    }
+}
