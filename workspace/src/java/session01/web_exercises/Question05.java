ae6e3f317a764784c886f8fb47b6e61047bae5db
NgThuy <ngothuy05111998@gmail.com>
2018-06-20 17:46:40 +0700

    Sec01,02,03,04

--- workspace/src/java/session01/web_exercises/Question05.java
+++
@@ -1,8 +0,0 @@
-package session01.web_exercises;
-//Web Exercise 5
-public class Question05 {
-    public static void main() // Bạn thiếu String[] args
-    {
-        System.out.println("Doesn't execute");
-    }
-}
--- workspace/src/java/session04/web_exercies/Question31.java
+++
@@ -1,9 +0,0 @@
-package session04.web_exercies;
-
-public class Question31 {
-    /*
-    * 31.What happens when you try to compile a program with the following statement?
-    * int[] a = new int[-17];
-    *Exception : java.lang.NegativeArraySizeException
-    * */
-}
