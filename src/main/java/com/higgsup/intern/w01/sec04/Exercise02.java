56e63602a703a30469ef3d09b5b341ecbe0aa814
DucThanh6111996 <Hoahongden123>
2018-06-21 00:05:49 +0700

    Exercise02

---
+++ src/main/java/com/higgsup/intern/w01/sec04/Exercise02.java
@@ -0,0 +1,9 @@
+package com.higgsup.intern.w01.sec04;
+
+public class Exercise02 {
+    public static void main(String[] args) {
+        int n=1000;
+        int[] a = new int[n*n*n*n];
+        System.out.println(a.length); // lỗi exception vì vượt quá kích thước mảng */
+    }
+}
