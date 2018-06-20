fc2f34d7399b4158164ed5ca5c9dc65752a8d634
Thieu Tu <tu.thieuthanh@gmail.com>
2018-06-20 17:04:52 +0700

    Complete sec04-Exercise 02!

---
+++ src/main/java/com/higgsup/intern/w01/sec04/Exercise02.java
@@ -0,0 +1,11 @@
+package com.higgsup.intern.w01.sec04;
+
+public class Exercise02 {
+    public static void main(String[] args) {
+        int n = 1000;
+        int[] a = new int[n*n*n*n];
+// Chương trình sẽ throw ra exception: java.lang.NegativeArraySizeException.
+// Có nghĩa là Kích thước của mảng có negative size, cụ thể là vượt quá kính thước kiểu integer
+
+    }
+}
