2640e02cd7d619bcd0f8cec8b62df9c1721f3ff2
tuanhiep <nguyennhutuanhiep@gmail.com>
2018-06-20 17:03:12 +0700

    How many variable

---
+++ src/main/java/com/higgsup/intern/w01/sec04/Exercise11.java
@@ -0,0 +1,10 @@
+package com.higgsup.intern.w01.sec04;
+
+public class Exercise11
+{
+    public static void main(String[] args)
+    {
+        int x = args.length;
+        System.out.println("Number of command line arguments: " + x);
+    }
+}
