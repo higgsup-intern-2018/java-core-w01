98dcd2df934ab770ce7309cac1d3d7affa3e35d3
tuanhiep <nguyennhutuanhiep@gmail.com>
2018-06-21 09:00:15 +0700

    Modify class GradeBook

---
+++ src/main/java/com/higgsup/intern/w01/sec05/Exercise1.java
@@ -0,0 +1,49 @@
+package com.higgsup.intern.w01.sec05;
+
+public class Exercise1
+{
+    private String courseName; // course name for this GradeBook
+    private String courseInstructor;    //course instructor
+
+    // constructor initializes courseName with String supplied as argument
+    // modified to specify two parameters for course name and instructor name
+    public Exercise1(String name, String instructor) {
+        courseName = name; // initializes courseName
+        courseInstructor = instructor;  //initializes courseInstructor
+    } // end constructor
+
+    // method to set the course name
+    public void setCourseName(String name) {
+        courseName = name; // store the course name
+    } // end method setCourseName
+
+    // method to retrieve the course name
+    public String getCourseName() {
+        return courseName;
+    } // end method getCourseName
+
+    //method to set the course instructor
+    public void setCourseInstructor(String instructor) {
+        courseInstructor = instructor;
+    }
+
+    //method to retrieve the course instructor
+    public String getCourseInstructor(){
+        return courseInstructor;
+    }
+
+    // display a welcome message to the GradeBook user
+    // modified output message
+    public void displayMessage() {
+        // this statement calls getCourseName to get the
+        // name of the course this GradeBook represents
+        System.out.printf("Welcome to the grade book for\n%s!\n", getCourseName());
+        System.out.println("This course is presented by: " + getCourseInstructor());
+    } // end method displayMessage
+
+    //Test application
+    public static void main(String[] args) {
+        Exercise1 exDemo= new Exercise1("JAVA", "Kev");
+        exDemo.displayMessage();
+    }
+}
