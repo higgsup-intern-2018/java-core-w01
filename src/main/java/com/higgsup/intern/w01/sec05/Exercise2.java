5ea6b34f293650594758f4fe683b52b53c8f5729
tuanhiep <nguyennhutuanhiep@gmail.com>
2018-06-21 09:16:03 +0700

    Modify class Account and AccountTest

---
+++ src/main/java/com/higgsup/intern/w01/sec05/Exercise2.java
@@ -0,0 +1,37 @@
+package com.higgsup.intern.w01.sec05;
+
+public class Exercise2
+{
+    private double balance; // instance variable that stores the balance
+
+    // constructor
+    public Exercise2(double initialBalance) {
+        // validate that initialBalance is greater than 0.0;
+        // if it is not, balance is initialized to the default value 0.0
+        if (initialBalance > 0.0) {
+            balance = initialBalance;
+        }
+    } // end Account constructor
+
+    // credit (add) an amount to the account
+    public void credit(double amount) {
+        balance = balance + amount; // add amount to balance
+    } // end method credit
+
+    // debit (withdraw) an amount from the account
+
+    public void debit(double amount)
+    {
+        if(balance < amount)
+        {
+            System.out.println("Debit amount exceed account balance");
+        }else {
+            balance -= amount;
+        }
+    }
+
+    // return the account balance
+    public double getBalance() {
+        return balance; // gives the value of balance to the calling method
+    } // end method getBalance
+}
---
+++ src/main/java/com/higgsup/intern/w01/sec05/Exercise2Test.java
@@ -0,0 +1,53 @@
+package com.higgsup.intern.w01.sec05;
+import java.util.Scanner;
+
+public class Exercise2Test
+{
+    public static void main(String args[]) {
+        Exercise2 account1 = new Exercise2(50.00); // create Account object
+        Exercise2 account2 = new Exercise2(-7.53); // create Account object
+
+        // display initial balance of each object
+        System.out.printf("account1 balance:$%.2f\n", account1.getBalance());
+        System.out.printf("account2 balance:$%.2f\n\n", account2.getBalance());
+
+        // create Scanner to obtain input from command window
+        Scanner input = new Scanner(System.in);
+
+        double depositAmount; // deposit amount read from user
+        System.out.print("Enter deposit amount for account1: "); // prompt
+        depositAmount = input.nextDouble(); // obtain user input
+        System.out.printf("\nadding %.2f to account1 balance\n\n", depositAmount);
+        account1.credit(depositAmount); // add to account1 balance
+
+        // display balances
+        System.out.printf("account1 balance:$%.2f\n", account1.getBalance());
+        System.out.printf("account2 balance:$%.2f\n\n", account2.getBalance());
+
+        System.out.print("Enter deposit amount for account2: "); // prompt
+        depositAmount = input.nextDouble(); // obtain user input
+        System.out.printf("\nadding %.2f to account2 balance\n\n", depositAmount);
+        account2.credit(depositAmount); // add to account2 balance
+
+        // display balances
+        System.out.printf("account1 balance:$%.2f\n", account1.getBalance());
+        System.out.printf("account2 balance:$%.2f\n", account2.getBalance());
+
+        double withdrawAmount;      //withdraw amount read from user
+        System.out.print("Enter withdraw amount for account1: ");
+        withdrawAmount = input.nextDouble();
+        System.out.printf("\nwithdrawing $%.2f to account1 balance\n\n", withdrawAmount);
+        account1.debit(withdrawAmount);     //withdraw from account 1 balance with amount smaller than balance
+
+        System.out.printf("account1 balance:$%.2f\n", account1.getBalance());
+        System.out.printf("account2 balance:$%.2f\n\n", account2.getBalance());
+
+        System.out.print("Enter withdraw amount for account2: ");
+        withdrawAmount = input.nextDouble();
+        System.out.printf("\nwithdrawing $%.2f to account2 balance\n\n", withdrawAmount);
+        account2.debit(withdrawAmount);     //withdraw from account 2 balance with amount exceed balance
+
+        System.out.printf("account1 balance:$%.2f\n", account1.getBalance());
+        System.out.printf("account2 balance:$%.2f\n\n", account2.getBalance());
+    } // end main
+}
