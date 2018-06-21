e7adb57976f3848732ef2d99677d1013a4f8a83e
Thieu Tu <tu.thieuthanh@gmail.com>
2018-06-21 10:38:52 +0700

    Add the method called debit!

---
+++ src/main/java/com/higgsup/intern/w01/sec05/part01/Account.java
@@ -0,0 +1,28 @@
+package com.higgsup.intern.w01.sec05.part01;
+
+public class Account {
+    private double balance;
+
+    public Account(double initialBalance) {
+        if (initialBalance > 0.0) {
+            balance = initialBalance;
+        }
+    }
+    // credit (add) an amount to the account
+    public void credit(double amount) {
+        balance = balance + amount; // add amount to balance
+    }
+
+    // return the account balance
+    public double getBalance() {
+        return balance;
+    }
+
+    //debit the account
+    public void debit(double amount){
+        if (amount >= getBalance()){
+            System.out.println("Debit amount exceeded account balance.");
+        } else System.out.println("Withdraw money successful!");
+    }
+
+}
