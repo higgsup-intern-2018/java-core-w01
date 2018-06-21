e0da43ac64c75b0f3aea869df2c2110c702ed430
Thieu Tu <tu.thieuthanh@gmail.com>
2018-06-21 11:42:55 +0700

    Create Employee class!

---
+++ src/main/java/com/higgsup/intern/w01/sec05/part01/Employee.java
@@ -0,0 +1,39 @@
+package com.higgsup.intern.w01.sec05.part01;
+
+public class Employee {
+    private String firstName;
+    private String lastName;
+    private double salary;
+
+    public Employee(String firstName, String lastName, double salary) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.salary = salary;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        if (salary < 0 ){
+            this.salary = 0.0;
+        } else this.salary = salary;
+    }
+}
