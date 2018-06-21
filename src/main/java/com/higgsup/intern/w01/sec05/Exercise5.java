d913d806759fe0be9b2c3bd069204c8bda9186a5
tuanhiep <nguyennhutuanhiep@gmail.com>
2018-06-21 14:36:44 +0700

    Exercise 5: Read from file

---
+++ src/main/java/com/higgsup/intern/w01/sec05/Exercise5.java
@@ -0,0 +1,20 @@
+package com.higgsup.intern.w01.sec05;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class Exercise5
+{
+    public static void main(String[] args) {
+        try {
+
+            Scanner input = new Scanner (new File("D:\\challenge.txt"));
+
+            while(input.hasNextLine())
+            {
+                String s = input.next();
+                System.out.print(s + " ");
+            }
+        } catch (java.io.FileNotFoundException e) {}
+    }
+}
