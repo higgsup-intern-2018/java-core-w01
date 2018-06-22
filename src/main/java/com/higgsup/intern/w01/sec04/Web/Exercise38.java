917d2714e781572fcadd7bb33dc10c0a62f8470b
DucThanh6111996 <Hoahongden123>
2018-06-22 17:23:34 +0700

    Exercise38

--- src/main/java/com/higgsup/intern/w01/sec04/Web/Exercise38.java
+++ src/main/java/com/higgsup/intern/w01/sec04/Web/Exercise38.java
@@ -7,7 +7,12 @@ public class Exercise38 {
         int [] a = new int[5];
         Random random = new Random();
         for(int i = 0; i< a.length ; i++){
-            a[i] = random.nextInt();
+            a[i] = random.nextInt(6);
+        }
+        if(a[0] == a[1] && a[0] == a[2] && a[0]== a[3] && a[0]==a[4] &&a[0] == a[5] ){
+            System.out.println("Yahtzee");
+        }else {
+            System.out.println("Try again");
         }
 
 
