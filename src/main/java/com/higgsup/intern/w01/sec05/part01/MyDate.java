acda6a92199e85a3fa720d679f99eab4c9d508a0
Thieu Tu <tu.thieuthanh@gmail.com>
2018-06-22 11:19:13 +0700

    remove main method in MyDate Class

--- src/main/java/com/higgsup/intern/w01/sec05/part01/MyDate.java
+++ src/main/java/com/higgsup/intern/w01/sec05/part01/MyDate.java
@@ -65,13 +65,5 @@ public class MyDate {
         }
     }
 
-
-    public static void main(String[] args) {
-        MyDate date1 = new MyDate(6, 1, 2018);
-        for (int day1 = 0; day1 < 300; day1++) {
-            date1.nextDay();
-            System.out.println(date1.toString());
-
-        }
-    }
 }
+
