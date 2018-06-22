c4857c4f25e95d68fff1353a533fdb6ed23665a6
Thieu Tu <tu.thieuthanh@gmail.com>
2018-06-22 14:39:40 +0700

    Create PeopleTest in Ex02 to test the classes written on Exercise 01

---
+++ src/main/java/com/higgsup/intern/w01/sec05/part02/PeopleTest.java
@@ -0,0 +1,20 @@
+package com.higgsup.intern.w01.sec05.part02;
+
+import com.higgsup.intern.w01.sec05.part02.model.Manager;
+import com.higgsup.intern.w01.sec05.part02.model.Employee;
+
+import java.util.Date;
+
+public class PeopleTest {
+    public static void main(String[] args) {
+        Employee newbie = new Employee ("Newbie", new Date("2/10/1989"), 1000000);
+        Manager boss = new Manager("Boss", new Date("2/23/1986"), 4000000);
+        boss.setAssistant(newbie);
+        Manager biggerBoss = new Manager("Big Boss", new Date("3/12/1969"), 1000000);
+        biggerBoss.setAssistant(boss);
+
+        System.out.println(newbie);
+        System.out.println(boss);
+        System.out.println(biggerBoss);
+    }
+}
