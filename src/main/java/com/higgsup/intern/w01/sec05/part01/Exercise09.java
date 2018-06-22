c807ef7f0b018433af2c357b9dc19d185e02de7a
Thieu Tu <tu.thieuthanh@gmail.com>
2018-06-22 11:20:00 +0700

    Test MyDate class

--- src/main/java/com/higgsup/intern/w01/sec05/part01/Exercise09.java
+++ src/main/java/com/higgsup/intern/w01/sec05/part01/Exercise09.java
@@ -2,6 +2,11 @@ package com.higgsup.intern.w01.sec05.part01;
 
 public class Exercise09 {
     public static void main(String[] args) {
+        MyDate date1 = new MyDate(6, 1, 2018);
+        for (int day1 = 0; day1 < 300; day1++) {
+            date1.nextDay();
+            System.out.println(date1.toString());
 
+        }
     }
 }
