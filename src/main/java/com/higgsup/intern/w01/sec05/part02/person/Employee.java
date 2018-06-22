c4f0254159a2837b5b85232c2f8c8cf93b28b5c7
Thieu Tu <tu.thieuthanh@gmail.com>
2018-06-22 14:51:21 +0700

    Move Person and Employee to the people package

---
+++ src/main/java/com/higgsup/intern/w01/sec05/part02/person/Employee.java
@@ -0,0 +1,21 @@
+package com.higgsup.intern.w01.sec05.part02.person;
+
+import java.util.Date;
+
+public class Employee extends Person{
+    private double salary;
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public Employee(String name, Date date, double salary) {
+        super(name, date, salary);
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return (super.toString() + "salary = " + salary );
+    }
+}
