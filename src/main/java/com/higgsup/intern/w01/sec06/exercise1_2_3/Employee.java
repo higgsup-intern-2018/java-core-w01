5c0242873fe2c1bf037bb0fb2a312dc077d1559f
tuanhiep <nguyennhutuanhiep@gmail.com>
2018-06-22 09:31:49 +0700

    Exercise 1,2,3

--- src/main/java/com/higgsup/intern/w01/sec06/exercise1_2_3/Employee.java
+++ src/main/java/com/higgsup/intern/w01/sec06/exercise1_2_3/Employee.java
@@ -2,7 +2,7 @@ package com.higgsup.intern.w01.sec06.exercise1_2_3;
 
 import java.util.Date;
 
-public class Employee extends People
+public class Employee extends Person
 {
     private double salary;
 
--- src/main/java/com/higgsup/intern/w01/sec06/exercise1_2_3/Person.java
+++ src/main/java/com/higgsup/intern/w01/sec06/exercise1_2_3/Person.java
@@ -1,4 +1,24 @@
 package com.higgsup.intern.w01.sec06.exercise1_2_3;
 
-public class Person {
+import java.util.Date;
+
+public class Person
+{
+    private String name;
+    private Date birthday;
+
+    public Person(String name, Date birthday)
+    {
+        this.name = name;
+        this.birthday = birthday;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String toString()
+    {
+        return "People information: \nName: " + name + "\nBirthday: " + birthday.toString();
+    }
 }
