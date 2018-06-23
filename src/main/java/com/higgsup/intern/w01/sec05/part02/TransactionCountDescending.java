af328af47cca1e98c88eabaf1713ce6aa5ca302c
Thieu Tu <tu.thieuthanh@gmail.com>
2018-06-24 00:12:27 +0700

    Ex06-sec05-part02: Edit less method in TransactionCountDescending

--- src/main/java/com/higgsup/intern/w01/sec05/part02/TransactionCountDescending.java
+++ src/main/java/com/higgsup/intern/w01/sec05/part02/TransactionCountDescending.java
@@ -5,6 +5,10 @@ import com.higgsup.intern.w01.sec05.part02.accounts.BankAccount;
 public class TransactionCountDescending implements MyComparator {
     @Override
     public boolean less(BankAccount a1, BankAccount a2) {
-        return false;
+        if (a1.getTransaction() < a2.getTransaction()){
+            return true;
+        }else{
+            return false;
+        }
     }
 }
