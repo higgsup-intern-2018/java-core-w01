f859928f5833dde3aefaf96c21e02de8af021220
Thieu Tu <tu.thieuthanh@gmail.com>
2018-06-24 00:13:30 +0700

    Ex06-sec05-part02: Edit less method in BalanceAscending

--- src/main/java/com/higgsup/intern/w01/sec05/part02/BalanceAscending.java
+++ src/main/java/com/higgsup/intern/w01/sec05/part02/BalanceAscending.java
@@ -5,6 +5,10 @@ import com.higgsup.intern.w01.sec05.part02.accounts.BankAccount;
 public class BalanceAscending implements MyComparator {
     @Override
     public boolean less(BankAccount a1, BankAccount a2) {
-        return false;
+        if (a1.getBalance() > a2.getBalance()){
+            return true;
+        }else {
+            return false;
+        }
     }
 }
