c7d5e51cac69f30c271a5e8da69e7236b931a37b
Thieu Tu <tu.thieuthanh@gmail.com>
2018-06-23 11:34:24 +0700

    Exercise06-sec05-part02: Create MyComparator Interface
    Add sort method

---
+++ src/main/java/com/higgsup/intern/w01/sec05/part02/MyComparator.java
@@ -0,0 +1,7 @@
+package com.higgsup.intern.w01.sec05.part02;
+
+import com.higgsup.intern.w01.sec05.part02.accounts.BankAccount;
+
+public interface MyComparator {
+    boolean less(BankAccount a1, BankAccount a2);
+}
