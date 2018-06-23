9b7e90be4aeb3af5d32c70a32bd432aab4524081
Thieu Tu <tu.thieuthanh@gmail.com>
2018-06-23 11:31:21 +0700

    Create InsertionSort Class

---
+++ src/main/java/com/higgsup/intern/w01/sec05/part02/InsertionSort.java
@@ -0,0 +1,9 @@
+package com.higgsup.intern.w01.sec05.part02;
+
+import com.higgsup.intern.w01.sec05.part02.accounts.BankAccount;
+
+public class InsertionSort {
+    public void sort(BankAccount[] account, MyComparator compare) {
+        //compare.less(account);
+    }
+}
