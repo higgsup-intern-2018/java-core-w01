1f7e1627dffc864b7a1ae2d86d776dfa518bb638
Thieu Tu <tu.thieuthanh@gmail.com>
2018-06-24 00:11:10 +0700

    Exercise06: Add body of sort method

--- src/main/java/com/higgsup/intern/w01/sec05/part02/InsertionSort.java
+++ src/main/java/com/higgsup/intern/w01/sec05/part02/InsertionSort.java
@@ -4,6 +4,18 @@ import com.higgsup.intern.w01.sec05.part02.accounts.BankAccount;
 
 public class InsertionSort {
     public void sort(BankAccount[] account, MyComparator compare) {
-        //compare.less(account);
+        int n = account.length;
+        for(int i = 0; i < n; i++){
+            for(int j = i; j > 0; j--){
+                if(compare.less(account[j], account[j-1])){
+                    swap(account, j, j - 1);
+                }
+            }
+        }
+    }
+    public static void swap(BankAccount[] account, int i, int j){
+        BankAccount temp = account[i];
+        account[i] = account[j];
+        account[j] = temp;
     }
 }
