f052696762640f1c41980a85053816017cfb7b84
Thieu Tu <tu.thieuthanh@gmail.com>
2018-06-24 11:20:49 +0700

    Ex06-sec05-part02: add successfulTransactionCount variable and override withdraw

--- src/main/java/com/higgsup/intern/w01/sec05/part02/accounts/NickelNDime.java
+++ src/main/java/com/higgsup/intern/w01/sec05/part02/accounts/NickelNDime.java
@@ -1,21 +1,23 @@
 package com.higgsup.intern.w01.sec05.part02.accounts;
 
 public class NickelNDime extends BankAccount {
-    private double withdrawalMoney;
-    private double balance;
-    private int sucessfulTransaction;
+    private int successfulTransactionCount;
 
     public NickelNDime(double balance) {
         super(balance);
-        this.balance = balance;
+    }
+
+    @Override
+    boolean withdraw(double withdrawalMoney) {
+        if(super.withdraw(withdrawalMoney)){
+            successfulTransactionCount++;
+            return true;
+        }else return false;
     }
 
     @Override
     double endMonthCharge() {
-        if (super.withdraw(withdrawalMoney) == true){
-            sucessfulTransaction++;
-            return 2000*sucessfulTransaction;
-        }
-        return 0;
+        return 2000 * successfulTransactionCount;
     }
+
 }
