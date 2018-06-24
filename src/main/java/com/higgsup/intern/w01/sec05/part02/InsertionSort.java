6de4f3fc48c7bb8becd8161e045182c71475ba70
Thieu Tu <tu.thieuthanh@gmail.com>
2018-06-24 11:24:13 +0700

    formatting

--- src/main/java/com/higgsup/intern/w01/sec05/part02/InsertionSort.java
+++ src/main/java/com/higgsup/intern/w01/sec05/part02/InsertionSort.java
@@ -5,15 +5,16 @@ import com.higgsup.intern.w01.sec05.part02.accounts.BankAccount;
 public class InsertionSort {
     public void sort(BankAccount[] account, MyComparator compare) {
         int n = account.length;
-        for(int i = 0; i < n; i++){
-            for(int j = i; j > 0; j--){
-                if(compare.less(account[j], account[j-1])){
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j > 0; j--) {
+                if (compare.less(account[j], account[j - 1])) {
                     swap(account, j, j - 1);
                 }
             }
         }
     }
-    public static void swap(BankAccount[] account, int i, int j){
+
+    public static void swap(BankAccount[] account, int i, int j) {
         BankAccount temp = account[i];
         account[i] = account[j];
         account[j] = temp;
