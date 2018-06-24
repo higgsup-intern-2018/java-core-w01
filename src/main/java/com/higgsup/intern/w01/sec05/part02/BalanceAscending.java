915f123e4f7e2268a344e858311cbc4890e2843f
Thieu Tu <tu.thieuthanh@gmail.com>
2018-06-24 11:22:31 +0700

    Change > to < in BalanceAscending

--- src/main/java/com/higgsup/intern/w01/sec05/part02/BalanceAscending.java
+++ src/main/java/com/higgsup/intern/w01/sec05/part02/BalanceAscending.java
@@ -5,7 +5,7 @@ import com.higgsup.intern.w01.sec05.part02.accounts.BankAccount;
 public class BalanceAscending implements MyComparator {
     @Override
     public boolean less(BankAccount a1, BankAccount a2) {
-        if (a1.getBalance() > a2.getBalance()){
+        if (a1.getBalance() < a2.getBalance()){
             return true;
         }else {
             return false;
