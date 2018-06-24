d313130caf0b42d83caf98f4f95d36389789f522
Thieu Tu <tu.thieuthanh@gmail.com>
2018-06-24 11:23:19 +0700

    Change < to > in TransactionCountDescending

--- src/main/java/com/higgsup/intern/w01/sec05/part02/TransactionCountDescending.java
+++ src/main/java/com/higgsup/intern/w01/sec05/part02/TransactionCountDescending.java
@@ -5,7 +5,7 @@ import com.higgsup.intern.w01.sec05.part02.accounts.BankAccount;
 public class TransactionCountDescending implements MyComparator {
     @Override
     public boolean less(BankAccount a1, BankAccount a2) {
-        if (a1.getTransaction() < a2.getTransaction()){
+        if (a1.getTransactionCount() > a2.getTransactionCount()){
             return true;
         }else{
             return false;
