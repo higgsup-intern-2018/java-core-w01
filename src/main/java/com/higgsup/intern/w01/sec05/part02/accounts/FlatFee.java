33de934256c6080fd308da7d76100e998123af66
Thieu Tu <tu.thieuthanh@gmail.com>
2018-06-24 11:17:57 +0700

    Delete balance variable

--- src/main/java/com/higgsup/intern/w01/sec05/part02/accounts/FlatFee.java
+++ src/main/java/com/higgsup/intern/w01/sec05/part02/accounts/FlatFee.java
@@ -1,11 +1,9 @@
 package com.higgsup.intern.w01.sec05.part02.accounts;
 
 public class FlatFee extends BankAccount{
-    private double balance;
 
     public FlatFee(double balance) {
         super(balance);
-        this.balance = balance;
     }
 
     @Override
