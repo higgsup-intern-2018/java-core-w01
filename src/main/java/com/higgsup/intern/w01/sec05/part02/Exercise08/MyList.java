6a88a872d4792a4dbcd39577c73d249e487df579
Thieu Tu <tu.thieuthanh@gmail.com>
2018-06-24 15:14:58 +0700

    Ex08-sec05-part02: Create MyList class

---
+++ src/main/java/com/higgsup/intern/w01/sec05/part02/Exercise08/MyList.java
@@ -0,0 +1,20 @@
+package com.higgsup.intern.w01.sec05.part02.Exercise08;
+
+public class MyList implements Item {
+    private Node start;
+    private Node end;
+
+    public MyList() {
+    }
+    public void append(Item item){
+
+    }
+    public void appendList(MyList myList){
+
+    }
+
+    @Override
+    public Item clone() {
+        return null;
+    }
+}
