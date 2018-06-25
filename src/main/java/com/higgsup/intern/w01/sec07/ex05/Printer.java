383f359e49cbe4018f74d5f9544a65e422c97a96
tuanhiep <nguyennhutuanhiep@gmail.com>
2018-06-25 14:46:59 +0700

    Exercise 5

--- src/main/java/com/higgsup/intern/w01/sec07/ex05/Printer.java
+++ src/main/java/com/higgsup/intern/w01/sec07/ex05/Printer.java
@@ -2,7 +2,27 @@ package com.higgsup.intern.w01.sec07.ex05;
 
 public class Printer
 {
-    public void printArray(int[] arr)
+    public void printInt(int[] arr)
+    {
+        System.out.print("[" + arr.length + "]:");
+        for(int i = 0; i < arr.length-1; i++)
+        {
+            System.out.print(arr[i]+", ");
+        }
+        System.out.println(arr[arr.length-1] + ";");
+    }
+
+    public void printChar(char[] arr)
+    {
+        System.out.print("[" + arr.length + "]:");
+        for(int i = 0; i < arr.length-1; i++)
+        {
+            System.out.print(arr[i]+", ");
+        }
+        System.out.println(arr[arr.length-1] + ";");
+    }
+
+    public void printString(String[] arr)
     {
         System.out.print("[" + arr.length + "]:");
         for(int i = 0; i < arr.length-1; i++)
--- src/main/java/com/higgsup/intern/w01/sec07/ex05/PrinterTest.java
+++ src/main/java/com/higgsup/intern/w01/sec07/ex05/PrinterTest.java
@@ -5,7 +5,10 @@ public class PrinterTest
     public static void main(String[] args) {
         Printer printer = new Printer();
         int[] nums = new int[]{1, 3, 5, 9, 6, 12, 14, 22, 16, 38};
-        printer.printArray(nums);
+        char[] ch = new char[]{'b', 'm', 's', 'd', 'f', 'h', 'k', 'l'};
+        String[] s = new String[]{"Ropz", "Stew", "tenz", "som"};
+        printer.printInt(nums);
+        printer.printChar(ch);
+        printer.printString(s);
     }
-
 }
