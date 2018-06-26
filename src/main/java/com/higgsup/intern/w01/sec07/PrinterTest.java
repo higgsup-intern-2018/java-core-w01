5f3a03fac040b3ccc925cd79afa9924e31f89c12
DucThanh6111996 <Hoahongden123>
2018-06-26 13:39:39 +0700

    Exercise05_sec7_Bonus

---
+++ src/main/java/com/higgsup/intern/w01/sec07/PrinterTest.java
@@ -0,0 +1,29 @@
+package com.higgsup.intern.w01.sec07;
+
+public class PrinterTest {
+    public static void main(String[] args) {
+        Printer printer = new Printer();
+        int[] number = new    int[]   {1,2,3,4,5,6};
+        char[] ch    = new   char[]   {'a', 'b', 'c', 'd', 'e', 'f'};
+        String[] str = new String[]   {"Bale", "Ronaldo", "Messi", "NeyMar", "Suarez","Matic"};
+
+        System.out.println("Không dùng generic");
+         printer.printInt(number);
+         printer.printChar(ch);
+         printer.printString(str);
+
+
+        System.out.println("Có dùng Generic");
+        PrinterGeneric<String> pg = new PrinterGeneric<>();
+        pg.printArray(str);
+
+        Character[] characters    = new   Character[]   {'a', 'b', 'c', 'd', 'e', 'f'};
+        PrinterGeneric IntergerPG = new PrinterGeneric();
+        IntergerPG.printArray(characters);
+
+        String[] strings = new String[]   {"Bale", "Ronaldo", "Messi", "NeyMar", "Suarez","Matic"};
+        PrinterGeneric StringPG = new PrinterGeneric();
+        StringPG.printArray(strings);
+
+    }
+}
