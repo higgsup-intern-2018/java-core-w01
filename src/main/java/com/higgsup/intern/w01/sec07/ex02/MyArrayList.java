8b0b0a6d95d1f14b7a4713cb383f3cbeb3044e0c
tuanhiep <nguyennhutuanhiep@gmail.com>
2018-06-26 11:06:09 +0700

    Exercise 2

--- src/main/java/com/higgsup/intern/w01/sec07/ex02/MyArrayList.java
+++ src/main/java/com/higgsup/intern/w01/sec07/ex02/MyArrayList.java
@@ -4,25 +4,28 @@ import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 
-public class MyList
+public class MyArrayList
 {
-    private List myList;
+    private List<String> myList;
 
-    public MyList(List list)
+    public MyArrayList(List<String> list)
     {
         myList = list;
     }
 
+    //add element
     public void add(String s)
     {
         myList.add(s);
     }
 
+    //print list
     public void print()
     {
         System.out.println(myList.toString());
     }
 
+    //iterate through all element
     public void iterate()
     {
         System.out.print("Iterate: ");
@@ -35,92 +38,103 @@ public class MyList
         System.out.println();
     }
 
+    //insert element to array list at first position
     public void insert(String color)
     {
         myList.add(0, color);
     }
 
-    public void update(int position, String update)
-    {
-        myList.set(position, update);
-    }
-
+    //retrieve element at given index
     public void retrieve(int index)
     {
         System.out.println(myList.get(index));
     }
 
+    //update specific array element by given element.
+    public void update(int position, String update)
+    {
+        myList.set(position, update);
+    }
+
+    //remove the third element from a array list.
     public void removeThird()
     {
         myList.remove(2);
     }
 
+    //search an element in a array list.
     public boolean searchElement(String color)
     {
-        for(Object c: myList)
-        {
-            if(c.toString().equalsIgnoreCase(color))
-            {
-                return true;
-            }
-        }
-        return false;
+        return myList.contains(color);
     }
 
+    //sort array list
     public void sortArray()
     {
         Collections.sort(myList);
     }
 
+    //copy one array list to another
     public void copy(List list)
     {
-        for(Object c : myList)
-        {
-            list.add(c);
-        }
+        Collections.copy(myList, list);
     }
 
+    //shuffle elements in array list
     public void shuffle()
     {
         Collections.shuffle(myList);
     }
 
+    //reverse elements in array list
     public void reverse()
     {
         Collections.reverse(myList);
     }
 
+    //compare two array list
     public boolean compare(List list)
     {
         return myList.equals(list);
     }
 
+    //join two array list
     public void join(List list)
     {
         myList.addAll(list);
     }
 
+    //clone array list to another array list
     public void clone(List clone)
     {
-       for(Object o : myList)
-           clone.add(o);
+       clone.addAll(myList);
     }
 
+    //empty array list
     public void emptyList()
     {
         myList.clear();
     }
 
+    //check if array list is empty or not
     public boolean isEmpty()
     {
         return myList.isEmpty();
     }
 
+    //replace second elements with specified element
+    public void replaceSecond(String elements)
+    {
+        update(1, elements);
+    }
+
+    //print all elements using the positions
     public void printAll()
     {
         for(int i = 0; i < myList.size(); i++)
         {
             System.out.print(myList.get(i) + " ");
         }
+        System.out.println();
     }
 }
--- src/main/java/com/higgsup/intern/w01/sec07/ex02/MyArrayListDemo.java
+++ src/main/java/com/higgsup/intern/w01/sec07/ex02/MyArrayListDemo.java
@@ -3,15 +3,15 @@ package com.higgsup.intern.w01.sec07.ex02;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MyListDemo
+public class MyArrayListDemo
 {
     public static void main(String[] args)
     {
         List myList = new ArrayList();
-        MyList list = new MyList(myList);
+        MyArrayList list = new MyArrayList(myList);
         List copied = new ArrayList<>();
         List clone = new ArrayList<>();
-        MyList anotherList = new MyList(copied);
+        MyArrayList anotherList = new MyArrayList(copied);
 
         //Add and print out collection
         list.add("Blue");
@@ -23,27 +23,31 @@ public class MyListDemo
         list.iterate();
 
         //retrieve element at a given index
+        System.out.print("Retrieve element at index 1: ");
         list.retrieve(1);
 
         //Insert element at first position
         list.insert("Purple");
+        System.out.print("After insert at first position: ");
         list.print();
-
 
         //Update specific array element by given element
         list.update(2, "Black");
+        System.out.print("After update specific element by given element: ");
         list.print();
 
         //remove the third element from a array list
         list.removeThird();
+        System.out.print("After remove third element: ");
         list.print();
 
         //search an element in an array list
-        System.out.println(list.searchElement("purple"));
-        System.out.println(list.searchElement("black"));
+        System.out.println("Is purple in array list?: " + list.searchElement("purple"));
+        System.out.println("Is black in array list?: " + list.searchElement("black"));
 
         //Sort a given array list
         list.sortArray();
+        System.out.print("After sort: ");
         list.print();
 
         //Copy one array list into another
@@ -54,34 +58,44 @@ public class MyListDemo
 
         //Shuffle elements in array list
         list.shuffle();
+        System.out.print("After shuffle: ");
         list.print();
 
         //Reverse elements in array list
         list.reverse();
+        System.out.print("After reverse at first position: ");
         list.print();
 
         //Compare two array list
-        System.out.println(list.compare(copied));
+        System.out.println("Is two array list equals?: " + list.compare(copied));
 
         //Join two array list
         list.join(copied);
+        System.out.print("After join: ");
         list.print();
-
 
         //Clone an array list to another array list
         list.clone(clone);
         System.out.print("Clone list: " + clone.toString());
         System.out.println();
 
+        //replace second element with specified element
+        list.replaceSecond("Sea");
+        System.out.print("After replace: ");
+        list.print();
+
+        //print all element using position of elements
+        System.out.print("Print all elements: ");
+        list.printAll();
+
         //Empty an array list
         list.emptyList();
+        System.out.print("After empty: ");
         list.print();
 
         //Check list is empty or not
-        System.out.println(list.isEmpty());
-        System.out.println(anotherList.isEmpty());
+        System.out.println("Is array list empty?: " + list.isEmpty());
 
-        //print all element using position of elements
-        anotherList.printAll();
+
     }
 }
